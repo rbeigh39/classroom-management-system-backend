9a9a67a8c0bac90452576076f8d8359bd32a0548
rbeigh39 <rbeigh39@gmail.com>
2022-09-01 21:42:36 +0530

    Initial Commit

---
+++ models/notificationModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  type: {
+    type: String,
+    enum: ["notification", "notice"],
+    default: "notification",
+  },
+  notificationTitle: {
+    type: String,
+    required: [true, "Please enter the notificationTitle"],
+  },
+  notificationMessage: {
+    type: String,
+    required: [true, "Please enter the notificationMessage"],
+  },
+  notificationType: {
+    type: String,
+    enum: ["general", "important", "critical"],
+    default: "general",
+  },
+});
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+module.exports = Notification;
