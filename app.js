5ce04376ecbc52e391d81096e09a0aaec9143349
rbeigh39 <rbeigh39@gmail.com>
2022-09-20 21:43:20 +0530

    Updated CORS allowed origins

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const app = express();
 //CORS
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     credentials: true,
   })
 );
