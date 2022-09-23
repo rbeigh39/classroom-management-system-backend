7a0d227671105e4adf87962ac51f6443a71c53a1
rbeigh39 <rbeigh39@gmail.com>
2022-09-23 09:07:38 +0530

    added CORS

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'http://127.0.0.1:3000',
-      'http://192.168.29.233',
+      'http://192.168.29.233:3000',
       '*',
     ],
     credentials: true,
