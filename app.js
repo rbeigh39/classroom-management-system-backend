33ee76d647330a74651a8f6a12159ba5ad037a2f
rbeigh39 <rbeigh39@gmail.com>
2022-10-06 16:30:32 +0530

    Updated CORS

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
+      'http://localhost:3001',
       'http://127.0.0.1:3001',
       'http://192.168.29.233:3000',
       'http://192.168.29.233:3001',
